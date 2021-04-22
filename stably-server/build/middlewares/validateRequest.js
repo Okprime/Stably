6efe9007e8d1dc82bdc0aad8e4ffb2540ca6afbb
Oghenekaro Uwede <uwedeoghenekaro@gmail.com>
2021-04-22 01:55:33 +0100

    Added an extra validation on the server

--- stably-server/build/middlewares/validateRequest.js
+++ stably-server/build/middlewares/validateRequest.js
@@ -11,6 +11,16 @@ const mainRequest_schemas_1 = require("../modules/mainRequest/mainRequest.schema
 exports.validateRequest = () => {
     return (req, _res, next) => {
         const result = mainRequest_schemas_1.bodySchema.validate(req.body);
+        console.log('resuly', result);
+        if ((req.body.number < 2) || (req.body.number < '2')) {
+            return next(createError_1.default(statuscode_1.HTTP.BAD_REQUEST, [
+                {
+                    status: response_1.Response.ERROR,
+                    message: 'Value input must be greater or equals to 2',
+                    data: null,
+                },
+            ]));
+        }
         if (result.error) {
             return next(createError_1.default(statuscode_1.HTTP.BAD_REQUEST, [
                 {
--- stably-server/src/middlewares/validateRequest.ts
+++ stably-server/src/middlewares/validateRequest.ts
@@ -14,6 +14,19 @@ export const validateRequest = () => {
 
   const result = bodySchema.validate(req.body);
 
+  // ensure on the server that the input must not be less than the lowest prime number (which is 2)
+  if ((req.body.number < 2) || (req.body.number < '2')) {
+    return next(
+      createError(HTTP.BAD_REQUEST, [
+        {
+          status: Response.ERROR,
+          message: 'Value input must be greater or equals to 2',
+          data: null,
+        },
+      ])
+    );
+  }
+
   if(result.error) {
     return next(
       createError(HTTP.BAD_REQUEST, [
